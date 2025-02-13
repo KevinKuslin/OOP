9f5a2e3a16af9a2143395026a65c42c6ffa4d6b8
KevinKuslin <tyrantnitrox@gmail.com>
2025-02-13 15:11:15 +0700

    Assignment 2

---
+++ src/Author.java
@@ -0,0 +1,18 @@
+
+public class Author {
+    private String author; 
+    private String biography; 
+
+    public Author(String author, String biography){
+        this.author = author; 
+        this.biography = biography; 
+    }
+
+    public String getAuthor(){
+        return author; 
+    }
+
+    public String getBiography(){
+        return biography;
+    }
+}
